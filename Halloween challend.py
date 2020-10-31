d004a5f9738f8e8b3904caa17355ef6e658da5bb
Chrisobz <73732021+Chrisobz@users.noreply.github.com>
2020-10-31 05:31:19 +0000

    Add files via upload

---
+++ Halloween challend.py
@@ -0,0 +1,31 @@
+print("THE ADDITION")
+a = int(input("ENTER A NUMBER: "))
+b = int(input("ENTER A NUMBER: "))
+print("the number will be added soon...")
+input("\n CLICK THE ENTER KEY TO PROCEED")
+c = (a+b)/2
+d = c + c
+e = d + d
+f = e/2
+g = f + f
+h = g/2
+i = h + h
+j = i/2
+k = j + j
+l = k/2
+m = l/3
+n = m+m+m
+o = n/4
+p = o+o+o+o
+q = p/20
+r = q+q+q+q+q+q+q+q+q+q+q+q+q+q+q+q+q+q+q+q
+s = r/40
+t = s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s
+u = t/60
+v = u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u+u
+w = v/100
+x = w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w+w
+y = x/200
+z = y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y+y
+print(round(z), " is the answer to the addition you have requested.")
+print("Thank you")
